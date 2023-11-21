09e60f78ac8696dcce01f8b2fd47991f6e8e2796
bilalashraf1710 <fakhar.mehdi@ocloudsolutions.net>
2023-11-21 20:03:41 +0500

    problem solved

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-const array= [
-    {id:1,name:"Bilal", city:"Lahore"},
-    {id:1,name:"Bilal", city:"Lahore"}
-    {id:3,name:"Hafsa", city:"Karachi"},
-    {id:4,name:"Rehan", city:"Lahore"},
-    {id:5,name:"Saqib", city:"Karachi"},
-    {id:6,name:"Farhan", city:"Islamabad"}
-    ]
-    
+const array = [{
+    id: 1,
+    name: "Bilal",
+    city: "Lahore"
+},
+{
+    id: 1,
+    name: "Bilal",
+    city: "Lahore"
+},
+{
+    id: 3,
+    name: "Hafsa",
+    city: "Karachi"
+},
+{
+    id: 4,
+    name: "Rehan",
+    city: "Lahore"
+},
+{
+    id: 5,
+    name: "Saqib",
+    city: "Karachi"
+},
+{
+    id: 6,
+    name: "Farhan",
+    city: "Islamabad"
+},
+]
+
+
+outputObj = {}
+
+for (let record of array) {
+city = record.city
+if (!outputObj[city])
+    outputObj[city] = []
+
+outputObj[city].push(record)
+
+}
+
+console.log(outputObj)
